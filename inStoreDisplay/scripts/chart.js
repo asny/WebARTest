0a6df1a56fec776e67538bddbcf5c7db0b3c104c
Magnus Koch <magnus.koch@alexandra.dk>
2017-11-14 16:35:51 +0100

    chart in div

--- inStoreDisplay/scripts/chart.js
+++ inStoreDisplay/scripts/chart.js
@@ -43,7 +43,7 @@ function Chart () {
 
 	this.drawEntries = function(entries, t) {
 
-		var canvas = document.getElementById('chartCanvas');
+		var canvas = document.getElementById('chart').getElementsByTagName('canvas')[0];
 		var context = canvas.getContext('2d');
 		context.clearRect(0, 0, canvas.width, canvas.height);
 
