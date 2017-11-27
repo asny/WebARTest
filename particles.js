f0afc934e03bf6df4a2b13c1c8fa6b19e9d3772d
Asger <asger.nyman@alexandra.dk>
2017-11-27 13:02:41 +0100

    Split into files

---
+++ particles.js
@@ -0,0 +1,75 @@
+
+function initParticles()
+{
+  // Particles
+  var material = new THREE.SpriteMaterial( {
+    map: new THREE.CanvasTexture( generateSprite() ),
+    blending: THREE.AdditiveBlending
+  } );
+
+  for ( var i = 0; i < 500; i++ ) {
+
+    particle = new THREE.Sprite( material );
+    particle.visible = false;
+    particles.push(particle);
+
+    scene.add( particle );
+  }
+}
+
+function generateSprite() {
+
+  var canvas = document.createElement( 'canvas' );
+  canvas.width = 16;
+  canvas.height = 16;
+
+  var context = canvas.getContext( '2d' );
+  var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
+  gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
+  gradient.addColorStop( 0.2, 'rgba(0,255,255,1)' );
+  gradient.addColorStop( 0.4, 'rgba(0,0,64,1)' );
+  gradient.addColorStop( 1, 'rgba(0,0,0,1)' );
+
+  context.fillStyle = gradient;
+  context.fillRect( 0, 0, canvas.width, canvas.height );
+
+  return canvas;
+
+}
+
+function initParticleEffect(pos)
+{
+  var time = 2000;
+  var delay = time / particles.length;
+  // Show particles
+  for(var i = 0; i < particles.length; i++)
+  {
+    initParticle( particles[i], delay, time, pos );
+  }
+}
+
+function initParticle( particle, delay, animationTime, pos ) {
+
+	var particle = this instanceof THREE.Sprite ? this : particle;
+
+  particle.visible = true;
+	particle.position.copy(pos);
+	particle.scale.x = particle.scale.y = 0.01 + 0.02 * Math.random();
+
+	new TWEEN.Tween( particle )
+		.delay( delay )
+		.to( {}, animationTime )
+		.onComplete( function(){particle.visible = false;} )
+		.start();
+
+	new TWEEN.Tween( particle.position )
+		.delay( delay )
+		.to( { x: pos.x + Math.random() - 0.5, y: pos.y + Math.random() - 0.5, z: pos.z + Math.random() - 0.5 }, animationTime )
+		.start();
+
+	new TWEEN.Tween( particle.scale )
+		.delay( delay )
+		.to( { x: 0.0001, y: 0.0001 }, animationTime )
+		.start();
+
+}
