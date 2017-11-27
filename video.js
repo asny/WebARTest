5c32463cf00368b5c8c483266f720991905b687f
Asger <asger.nyman@alexandra.dk>
2017-11-27 15:51:20 +0100

    Remove unused code

--- video.js
+++ video.js
@@ -11,14 +11,6 @@ function createVideo()
 	video.load(); // must call after setting/changing source
 	video.play();
 
-	// alternative method --
-	// create DIV in HTML:
-	// <video id="myVideo" autoplay style="display:none">
-	//		<source src="videos/sintel.ogv" type='video/ogg; codecs="theora, vorbis"'>
-	// </video>
-	// and set JS variable:
-	// video = document.getElementById( 'myVideo' );
-
 	var videoImage = document.createElement( 'canvas' );
 	videoImage.width = 480;
 	videoImage.height = 204;
@@ -33,12 +25,6 @@ function createVideo()
 	videoTexture.magFilter = THREE.LinearFilter;
 
 	var movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side:THREE.DoubleSide } );
-	// the geometry on which the movie will be displayed;
-	// 		movie image will be scaled to fit these dimensions.
-	/*var movieGeometry = new THREE.PlaneGeometry( 240, 100, 4, 4 );
-	var movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
-	movieScreen.position.set(0,50,0);
-	scene.add(movieScreen);*/
 
   var plane = new THREE.PlaneGeometry( 0.5, 0.5, 32, 32 );
 
@@ -49,35 +35,10 @@ function createVideo()
   mesh.position.copy(posWorld);
   mesh.quaternion.copy(rot);
   scene.add(mesh);
-
-  /*video = document.getElementById( 'video' );
-
-  videoImage = document.createElement( 'canvas' );
-  videoImage.width = 480;
-  videoImage.height = 204;
-
-  videoContext = videoImage.getContext( '2d' );
-  videoContext.fillStyle = '#000000';
-  videoContext.fillRect( 0, 0, 480, 204 );
-
-  videoTexture = new THREE.Texture( videoImage );
-  var material = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: 0.5 } );
-
-	var plane = new THREE.PlaneGeometry( 0.5, 0.5, 32, 32 );
-
-  var position = new THREE.Vector3(0.0, 0.5, 0.0);
-  var posWorld = localToWorld(position);
-
-	var mesh = new THREE.Mesh( plane, material );
-  mesh.position.copy(posWorld);
-  mesh.quaternion.copy(rot);
-  //mesh.rotation.x -= 0.5 * Math.PI;
-	scene.add(mesh);*/
 }
 
 function updateVideo()
 {
-  console.log("Damn " + video.readyState + ": " + video.HAVE_ENOUGH_DATA);
   // Update video
   if ( video.readyState === video.HAVE_ENOUGH_DATA )
 	{
