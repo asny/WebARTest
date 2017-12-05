89e7e7253c4534602a25804c83427de9575719f9
Asger <asger.nyman@alexandra.dk>
2017-12-05 11:16:43 +0100

    Transparency

--- particles.js
+++ particles.js
@@ -44,9 +44,9 @@ function generateBlueSprite() {
   var context = canvas.getContext( '2d' );
   var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
   gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
-  gradient.addColorStop( 0.2, 'rgba(0,255,255,1)' );
-  gradient.addColorStop( 0.4, 'rgba(0,0,64,1)' );
-  gradient.addColorStop( 1, 'rgba(0,0,0,1)' );
+  gradient.addColorStop( 0.2, 'rgba(0,255,255,0.8)' );
+  gradient.addColorStop( 0.4, 'rgba(0,0,64,0.2)' );
+  gradient.addColorStop( 1, 'rgba(0,0,0,0)' );
 
   context.fillStyle = gradient;
   context.fillRect( 0, 0, canvas.width, canvas.height );
@@ -63,9 +63,9 @@ function generateRedSprite() {
   var context = canvas.getContext( '2d' );
   var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
   gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
-  gradient.addColorStop( 0.2, 'rgba(255,255,0,1)' );
-  gradient.addColorStop( 0.4, 'rgba(64,0,0,1)' );
-  gradient.addColorStop( 1, 'rgba(0,0,0,1)' );
+  gradient.addColorStop( 0.2, 'rgba(255,255,0,0.8)' );
+  gradient.addColorStop( 0.4, 'rgba(64,0,0,0.2)' );
+  gradient.addColorStop( 1, 'rgba(0,0,0,0)' );
 
   context.fillStyle = gradient;
   context.fillRect( 0, 0, canvas.width, canvas.height );
