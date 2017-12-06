bac685017f147503e68aa02eb6b36575801d0d34
Asger <asger.nyman@alexandra.dk>
2017-12-06 08:51:18 +0100

    Video copy working on phone.

--- video.js
+++ video.js
@@ -1,5 +1,5 @@
 
-var texture, videoImageContext, video;
+var videoTexture, video;
 var posWorld;
 
 function createVideo()
@@ -13,40 +13,22 @@ function createVideo()
 		var url	= 'assets/sintel.ogv'
 	}else	alert('cant play mp4 or ogv')
 
-	// create the video element
-	video	= document.createElement('video');
-	video.width	= 320;
-	video.height	= 240;
-	video.autoplay	= true;
-  video.muted = true;
-	video.loop	= true;
-	video.src	= url;
+	// create the videoTexture
+	videoTexture= new THREEx.VideoTexture(url)
+	video	= videoTexture.video
+	updateFcts.push(function(delta, now){
+		videoTexture.update(delta, now)
+	})
 
-	// create the texture
-	texture	= new THREE.Texture( video );
+	// use the texture in a THREE.Mesh
+	var geometry	= new THREE.CubeGeometry(1,1,1);
+	var material	= new THREE.MeshBasicMaterial({
+		map	: videoTexture.texture
+	});
+	var mesh	= new THREE.Mesh( geometry, material );
+	scene.add( mesh );
 
-  // create the video element
-	/*video = document.createElement( 'video' );
-	// video.id = 'video';
-	// video.type = ' video/ogg; codecs="theora, vorbis" ';
-	video.src = "assets/sintel.mp4";
-  video.muted = true;
-	video.playsinline = true;
-	video.load(); // must call after setting/changing source
-	//video.play();
-
-	video = document.getElementById( 'myVideo' );
-
-	var videoImage = document.createElement( 'canvas' );
-	videoImage.width = 480;
-	videoImage.height = 204;
-
-	videoImageContext = videoImage.getContext( '2d' );
-	// background color if no video present
-	videoImageContext.fillStyle = '#ff0000';
-	videoImageContext.fillRect( 0, 0, videoImage.width, videoImage.height );*/
-
-	var material = new THREE.MeshBasicMaterial( { map: texture } );
+	/*var material = new THREE.MeshBasicMaterial( { map: texture } );
 
   var plane = new THREE.PlaneGeometry( 0.5, 0.5, 32, 32 );
 
@@ -56,7 +38,7 @@ function createVideo()
 	var mesh = new THREE.Mesh( plane, material );
   mesh.position.copy(posWorld);
   mesh.quaternion.copy(rot);
-  scene.add(mesh);
+  scene.add(mesh);*/
 
 	/*var button = document.createElement( 'button' );
 	var t = document.createTextNode("CLICK ME");
@@ -68,9 +50,23 @@ function createVideo()
 	document.body.appendChild(button);*/
 }
 
+
+function onVideoPlayButtonClick(){
+	video.play();
+}
+function onVideoPauseButtonClick(){
+	video.pause();
+}
+
+var lastTimeMsec = null;
 function updateVideo(pos)
 {
-	if( video.readyState !== video.HAVE_ENOUGH_DATA )
+	// measure time
+	lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
+	var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
+	lastTimeMsec	= nowMsec
+	videoTexture.update(delta, now);
+	/*if( video.readyState !== video.HAVE_ENOUGH_DATA )
 		return;
 	texture.needsUpdate	= true;
 	/*var shouldPause = pos.distanceTo(posWorld) > 2.0;
