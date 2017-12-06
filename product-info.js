8c9a1808a276101847645b2753884f7903fd7cb2
Asger <asger.nyman@alexandra.dk>
2017-12-06 12:47:16 +0100

    Product info

---
+++ product-info.js
@@ -0,0 +1,12 @@
+var productInfos =[
+   {
+      "title": "Boody boxers",
+      "text": "Boody Boxers er lavet af bambus og er både allergivenlig og god for miljøet!",
+      "img": "assets/logo1.png",
+   },
+   {
+      "title": "Boody boxers",
+      "text": "Boody Boxers er lavet af bambus og er både allergivenlig og god for miljøet!",
+      "img": "assets/hero-img.jpg",
+   }
+]
