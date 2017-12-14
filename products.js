721145607be1c7ba356d9ceef7a17b70c5ad0ff6
Asger <asger.nyman@alexandra.dk>
2017-12-14 13:22:02 +0100

    Updated products

--- products.js
+++ products.js
@@ -7,10 +7,10 @@ function createProducts()
   createProduct(new THREE.Vector3(0.3, 0.4, -0.1), productInfos[0]);
   //createProduct(new THREE.Vector3(0.6, 0.4, -0.1), productInfos[1]);
   //createProduct(new THREE.Vector3(0.3, 0.8, -0.15), productInfos[2]);
-  createProduct(new THREE.Vector3(0.6, 0.8, -0.15), productInfos[1]);
+  //createProduct(new THREE.Vector3(0.6, 0.8, -0.15), productInfos[1]);
   //createProduct(new THREE.Vector3(0.3, 1.2, -0.15), productInfos[4]);
-  createProduct(new THREE.Vector3(0.6, 1.2, -0.15), productInfos[2]);
-  createProduct(new THREE.Vector3(0.3, 1.6, -0.20), productInfos[3]);
+  //createProduct(new THREE.Vector3(0.6, 1.2, -0.15), productInfos[2]);
+  //createProduct(new THREE.Vector3(0.3, 1.6, -0.20), productInfos[3]);
   //createProduct(new THREE.Vector3(0.6, 1.6, -0.20), productInfos[7]);*/
 
   createParticleEffect(localToWorld(new THREE.Vector3(-0.5, 0.8, 0.3)));
@@ -32,6 +32,26 @@ function createProduct(position, productInfo)
   mesh.scale.set(0.0001, 0.0001, 0.0001);
   scene.add( mesh );
 
+  // VIDEO
+  var p2 = localToWorld(position.clone().add(new THREE.Vector3(0.0, 0.0, 0.04)));
+  createVideo(productInfo.vid, p2);
+
+  certificates = [];
+  for(var i = 0; i < productInfo.cers.length; i++)
+  {
+    var pos = localToWorld(position.clone().add(new THREE.Vector3(i * 0.1, 0.0, 0.04)));
+    certificate = createCertificate(pos, certifications[productInfo.cers[i]]);
+    certificates.push(certificate);
+  }
+
+  // Save product information
+  var product = {anchor:mesh, certificates:certificates, animation:0.0};
+  products.push(product);
+
+}
+
+function createCertificate(position, certificateInfo)
+{
   // TEXT
   // Create image
   var bitmap = document.createElement('canvas');
@@ -51,7 +71,7 @@ function createProduct(position, productInfo)
   div.style = "display:none;";
 
   var image = document.createElement("img");
-  image.src = productInfo.img;
+  image.src = certificateInfo.img;
   image.width = 3840;
   image.height = 2160;
   var imageHeight = image.height * textWidth / image.width;
@@ -63,14 +83,14 @@ function createProduct(position, productInfo)
   marginY += imageHeight + lineHeight * 2;
   context.font = '56px Open Sans';
   context.fillStyle = 'black';
-  marginY = wrapText(context, productInfo.title, marginX, marginY, textWidth, lineHeight);
+  marginY = wrapText(context, certificateInfo.title, marginX, marginY, textWidth, lineHeight);
 
   // Draw teaser
   lineHeight = 40;
   marginY += lineHeight;
   context.font = '28px Open Sans';
   context.fillStyle = 'black';
-  marginY = wrapText(context, productInfo.text, marginX, marginY, textWidth, lineHeight);
+  marginY = wrapText(context, certificateInfo.text, marginX, marginY, textWidth, lineHeight);
 
   // Draw certifications
   /*marginX += lineHeight * 0.25;
@@ -99,14 +119,11 @@ function createProduct(position, productInfo)
   var geometry = new THREE.PlaneGeometry( 0.25, 0.25, 8, 8 );
   var material = new THREE.MeshBasicMaterial( {map : texture, side: THREE.DoubleSide, transparent: true, opacity: 0.8} );
   var textGeometry = new THREE.Mesh( geometry, material );
-  var p2 = localToWorld(position.clone().add(new THREE.Vector3(0.0, 0.0, 0.04)));
-  textGeometry.position.copy(p2);
+  textGeometry.position.copy(position);
   textGeometry.quaternion.copy(rot);
   scene.add( textGeometry );
 
-  // Save product information
-  var product = {anchor:mesh, description:textGeometry, animation:0.0};
-  products.push(product);
+  return {description:textGeometry}
 }
 
 var lastTime = new Date().getTime();
@@ -130,9 +147,13 @@ function updateProducts(pos)
       var product = products[i];
       var animation = product.animation + 0.001 * currentTime * ( closestProduct == i ? 1.0 : - 1.0 );
       product.animation = Math.min(1.0, Math.max(0.0, animation));
-      product.description.scale.set(product.animation, product.animation, product.animation);
 
-      product.description.visible = product.animation > 0.00001;
+      for(var j = 0; j < product.certificates.length; j++)
+      {
+        var certificate = product.certificates[j];
+        certificate.description.scale.set(product.animation, product.animation, product.animation);
+        certificate.description.visible = product.animation > 0.00001;
+      }
 
       product.anchor.rotation.x += 0.015;
     	product.anchor.rotation.y += 0.01;
--- video.js
+++ video.js
@@ -1,30 +1,32 @@
 
-var videoTexture, video;
+var videoTexture = [];
+var video;
 var posWorld;
 
 function createVideo(url, position)
 {
 	// find out which file formats i can read
 	var canPlayMp4	= document.createElement('video').canPlayType('video/mp4') !== '' ? true : false
-	if( !canPlayMp4 )
+	if( canPlayMp4 )
 	{
-		alert('cant play mp4')
+		// create the videoTexture
+		videoTexture.push(new THREEx.VideoTexture(url));
+		video	= videoTexture.video;
+
+		var material = new THREE.MeshBasicMaterial( { map: videoTexture.texture, side: THREE.DoubleSide } );
+
+	  var plane = new THREE.PlaneGeometry( 0.5, 0.5, 32, 32 );
+
+	  posWorld = localToWorld(position);
+
+		var mesh = new THREE.Mesh( plane, material );
+	  mesh.position.copy(posWorld);
+	  mesh.quaternion.copy(rot);
+	  scene.add(mesh);
 	}
-
-	// create the videoTexture
-	videoTexture= new THREEx.VideoTexture(url)
-	video	= videoTexture.video
-
-	var material = new THREE.MeshBasicMaterial( { map: videoTexture.texture, side: THREE.DoubleSide } );
-
-  var plane = new THREE.PlaneGeometry( 0.5, 0.5, 32, 32 );
-
-  posWorld = localToWorld(position);
-
-	var mesh = new THREE.Mesh( plane, material );
-  mesh.position.copy(posWorld);
-  mesh.quaternion.copy(rot);
-  scene.add(mesh);
+	else {
+			alert('cant play mp4')
+	}
 }
 
 /*function onVideoPlayButtonClick(){
@@ -36,5 +38,7 @@ function onVideoPauseButtonClick(){
 
 function updateVideo(pos)
 {
-	videoTexture.update();
+	for(var i = 0; i < videoTexture.length; i++) {
+		videoTexture[i].update();
+	}
 }
