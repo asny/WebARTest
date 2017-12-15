0dc2dc611c00eceaf1b320e55db4ee73e8b0691a
Asger <asger.nyman@alexandra.dk>
2017-12-15 14:58:29 +0100

    Changed anchor position

--- products.js
+++ products.js
@@ -21,7 +21,7 @@ function createProduct(productInfo)
 
   // ANCHOR
   // Create anchor
-  var posAnchor = localToWorld(position.clone().add(new THREE.Vector3(-0.1, 0.1, 0.1)));
+  var posAnchor = localToWorld(position.clone().add(new THREE.Vector3(-0.2, 0.1, 0.05)));
   var mesh = anchorModel.clone();
   mesh.position.copy(posAnchor);
   mesh.quaternion.copy(rot);
